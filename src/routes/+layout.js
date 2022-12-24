51c1143633866bf3117a9c442b1af35f6930e89b
Brent Vanwildemeersch <brent.vanwildemeersch@easyfairs.com>
2022-12-24 11:22:50 +0100

    feat(Build): adjust sveltekit settings and adapter for static page build

---
+++ src/routes/+layout.js
@@ -0,0 +1,10 @@
+// Documentation: https://kit.svelte.dev/docs/page-options#prerender
+export const prerender = true;
+
+// if you want to Generate a SPA
+// Documentation: https://kit.svelte.dev/docs/page-options#ssr
+export const ssr = true;
+
+// How to manage the trailing slashes in the URLs
+// https://kit.svelte.dev/docs/page-options#trailingslash
+export const trailingSlash = 'ignore';
