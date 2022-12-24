e706018375d2ffada80d45d400b0758587e697a7
Brent Vanwildemeersch <brent.vanwildemeersch@easyfairs.com>
2022-12-24 11:18:06 +0100

    feat(Background): add background image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,12 @@
 <script>
   import "../app.css";
-  import Logo from '$lib/assets/logo.svg'
+  import Logo from '$lib/assets/logo.svg';
+  import Bg from '$lib/assets/bg.jpg';
 </script>
 
 <div class="w-screen h-screen relative">
-  <img src={Logo} class="m-auto w-1/2 logo" alt="logo"/>
+  <img src={Bg} class="absolute w-screen h-screen left-0 top-0 opacity-75" alt="background"/>
+  <img src={Logo} class="m-auto w-1/3 logo" alt="logo"/>
 </div>
 
 <style>
