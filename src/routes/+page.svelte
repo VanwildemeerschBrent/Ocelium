38614a9992de5415240e8f615347c34000d8d152
Brent Vanwildemeersch <brent.vanwildemeersch@easyfairs.com>
2022-12-29 14:21:25 +0100

    fix(Style): fix responsive styling for mobile

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,9 +4,9 @@
   import Bg from '$lib/assets/bg.jpg';
 </script>
 
-<div class="w-screen h-screen relative">
-  <img src={Bg} class="absolute w-screen h-screen left-0 top-0 opacity-75" alt="background"/>
-  <img src={Logo} class="m-auto w-1/3 logo" alt="logo"/>
+<div class="w-screen h-screen relative overflow-hidden">
+  <img src={Bg} class="absolute w-screen h-screen left-0 top-0 opacity-75 object-cover" alt="background"/>
+  <img src={Logo} class="m-auto w-60 md:w-1/3 logo" alt="logo"/>
 </div>
 
 <style>
